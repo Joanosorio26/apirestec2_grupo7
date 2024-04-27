cd69aaa6b807f9fd61ec6eaeea409ca2f5253dc3
joan <i202214610@cibertec.edu.pe>
2024-04-27 13:34:21 -0500

    inicio

---
+++ src/test/java/pe/edu/cibertec/apirestec2_grupo7/Apirestec2Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apirestec2_grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apirestec2Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
